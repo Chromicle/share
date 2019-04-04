3af7b50b3935e62886b6d219157e4bc75fc2a89a
prabhakar369 <ajayprabhakar369@gmail.com>
2019-04-04 21:24:00 +0530

    Added the flash feature

---
+++ skunkworks_crow/src/main/java/org/odk/share/activities/scannerActivity.java
@@ -0,0 +1,125 @@
+package org.odk.share.activities;
+/**
+ * created by Chromicle
+ */
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import org.odk.share.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+
+public class scannerActivity extends AppCompatActivity implements
+        DecoratedBarcodeView.TorchListener {
+
+    @BindView(R.id.zxing_barcode_scanner)
+    DecoratedBarcodeView barcodeView;
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+    private Button switchFlashlightButton;
+    private boolean isFlashLightOn = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        //Initialize barcode scanner view
+        barcodeScannerView = findViewById(R.id.zxing_barcode_scanner);
+
+        //set torch listener
+        barcodeScannerView.setTorchListener(this);
+
+        //switch flashlight button
+        switchFlashlightButton = (Button) findViewById(R.id.switch_flashlight);
+
+        // if the device does not have flashlight in its camera,
+        // then remove the switch flashlight button...
+        if (!hasFlash()) {
+            switchFlashlightButton.setVisibility(View.GONE);
+        } else {
+            switchFlashlightButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    switchFlashlight();
+                }
+            });
+        }
+
+
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
+
+    }
+
+
+    /**
+     * Check if the device's camera has a Flashlight.
+     *
+     * @return true if there is Flashlight, otherwise false.
+     */
+    private boolean hasFlash() {
+        return getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+    public void switchFlashlight() {
+        if (isFlashLightOn) {
+            barcodeScannerView.setTorchOff();
+            isFlashLightOn = false;
+        } else {
+            barcodeScannerView.setTorchOn();
+            isFlashLightOn = true;
+        }
+
+    }
+
+    @Override
+    public void onTorchOn() {
+        switchFlashlightButton.setBackgroundResource(R.drawable.ic_flash_on_black_24dp);
+
+    }
+
+    @Override
+    public void onTorchOff() {
+        switchFlashlightButton.setBackgroundResource(R.drawable.ic_flash_off_black_24dp);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+}
